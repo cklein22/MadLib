e30403abed0ae3c8dca5886be7e8d5fba836d05b
cklein22 <56313846+cklein22@users.noreply.github.com>
2019-11-20 08:52:55 -0500

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,21 @@
+"""
+Lake Madlib written by Cecelia Klein
+"""
+#here are the variables
+noun= raw_input("Please enter a noun: ")
+name= raw_input("Please enter a name: ")
+verb= raw_input("Please enter a verb ending in -ing: ")
+adjective= raw_input("Please enter an adjective: ")
+adjective2= raw_input("Please enter another adjective: ")
+feeling= raw_input("Please enter a feeling: ")
+feeling2= raw_input("Please enter a feeling in the past tense: ")
+place= raw_input("Please enter a place: ")
+
+#here is the story:
+print ("There once was a " + noun + " named " + name + " who loved to go to the lake. ")
+print ("One day, " + name + " decided to go " + verb + ". ")
+print ("The " + noun + " had a " + adjective + " time " + verb + ". ")
+print ("The next day a " + adjective2 + " storm came and " + name + " had to stay in the " + place + ". ")
+print ("When the " + noun + " went back to the lake the next day, the " + noun + " was " + feeling + " to be there. ")
+print ("The " + noun + " then decided to go back to the lake everyday because he realized how much he " + feeling2 + " it there. ")
+print ("The end.")
